4568ffbbab3c5c284d691b9ef38c2c4783bb7c57
Gabriel Sanchez <124187943+InTheLittleMoon@users.noreply.github.com>
2023-11-05 15:28:29 -0800

    wip

--- src/components/MainPage/MainPage.js
+++ src/components/MainPage/MainPage.js
@@ -6,12 +6,18 @@ import axiosInstance from "../../axiosInstance";
 
 //components
 import Posts from "../Posts/Posts";
+import CommentThread from "../CommentThread/CommentThread";
+import AddCommentPopup from "../AddCommentPopup/AddCommentPopup";
 
 function MainPage({ currentUser }) {
   // states
   const [createPostTrigger, setCreatePostTrigger] = useState(false);
   const [textInputValue, setTextInputValue] = useState("");
   const [createdPostsContainer, setCreatedPostsContainer] = useState([]);
+  const [makeCommentTrigger, setMakeCommentTrigger] = useState(false);
+  const [commentThreadTrigger, setCommentThreadTrigger] = useState(false);
+  const [addCommentData, setAddCommentData] = useState("");
+  const [postComments, setPostComments] = useState([]);
 
   // animation state
   const [fadeOutAnimation, setFadeOutAnimation] = useState(false);
@@ -78,76 +84,117 @@ function MainPage({ currentUser }) {
     fetchPosts();
   }, []);
 
+  // deals with getting list of posts
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const response = await axiosInstance.get("/posts");
+        if (response.data.success) {
+          console.log(response.data);
+          // setCreatedPostsContainer(response.data.posts);
+        } else {
+          console.log("Failed to fetch posts");
+        }
+      } catch (error) {
+        console.error("Error fetching posts:", error);
+      }
+    };
+
+    fetchPosts();
+  }, []);
+
   return (
-    <div className="main-container">
-      <div className="user-container">
-        <div className="user-info-container">
-          <div className="user-details">
-            <h1>{currentUser.username}</h1>
-            <h3>
-              {createdPostsContainer.length}{" "}
-              {createdPostsContainer.length === 1 ? "post" : "posts"}
-            </h3>
+    <>
+      {/* shouold have comment maker popup when triggered */}
+      {makeCommentTrigger && (
+        <AddCommentPopup
+          setMakeCommentTrigger={setMakeCommentTrigger}
+          addCommentData={addCommentData}
+          setAddCommentData={setAddCommentData}
+          setPostComments={setPostComments}
+        />
+      )}
+      {/* should show thread once trigger activated */}
+      {commentThreadTrigger && (
+        <CommentThread
+          postComments={postComments}
+          setCommentThreadTrigger={setCommentThreadTrigger}
+          setPostComments={setPostComments}
+        />
+      )}
+      <div className="main-container">
+        <div className="user-container">
+          <div className="user-info-container">
+            <div className="user-details">
+              <h1>{currentUser.username}</h1>
+              <h3>
+                {createdPostsContainer.length}{" "}
+                {createdPostsContainer.length === 1 ? "post" : "posts"}
+              </h3>
+            </div>
+            <div className="post-creator">
+              {!createPostTrigger && (
+                <button
+                  onClick={() => {
+                    postCreatorHandler();
+                  }}
+                >
+                  Create New Post
+                </button>
+              )}
+              {createPostTrigger && (
+                <button
+                  onClick={() => {
+                    cancelPostCreatorHandler();
+                  }}
+                >
+                  Cancel Create Post
+                </button>
+              )}
+            </div>
           </div>
-          <div className="post-creator">
-            {!createPostTrigger && (
-              <button
-                onClick={() => {
-                  postCreatorHandler();
-                }}
-              >
-                Create New Post
-              </button>
-            )}
-            {createPostTrigger && (
-              <button
-                onClick={() => {
-                  cancelPostCreatorHandler();
-                }}
-              >
-                Cancel Create Post
-              </button>
-            )}
-          </div>
-        </div>
-        {createPostTrigger && (
-          <div
-            className={`post-creator-block ${
-              fadeOutAnimation ? "fade-out" : "fade-in-slide-down"
-            }`}
-            onAnimationEnd={() => {
-              if (fadeOutAnimation) {
-                setCreatePostTrigger(false);
-                setFadeOutAnimation(false);
-                setTextInputValue("");
-              }
-            }}
-          >
-            <textarea
-              placeholder="What's on your mind?"
-              value={textInputValue}
-              onChange={(event) => {
-                postTextInputHandler(event);
-              }}
-            />
-            <button
-              onClick={() => {
-                createNewPost(textInputValue);
+          {createPostTrigger && (
+            <div
+              className={`post-creator-block ${
+                fadeOutAnimation ? "fade-out" : "fade-in-slide-down"
+              }`}
+              onAnimationEnd={() => {
+                if (fadeOutAnimation) {
+                  setCreatePostTrigger(false);
+                  setFadeOutAnimation(false);
+                  setTextInputValue("");
+                }
               }}
             >
-              Post
-            </button>
-          </div>
-        )}
+              <textarea
+                placeholder="What's on your mind?"
+                value={textInputValue}
+                onChange={(event) => {
+                  postTextInputHandler(event);
+                }}
+              />
+              <button
+                onClick={() => {
+                  createNewPost(textInputValue);
+                }}
+              >
+                Post
+              </button>
+            </div>
+          )}
+        </div>
+        <div className="user-posts-container">
+          {/* should have window extend when someone wants to comment */}
+          <Posts
+            createdPostsContainer={createdPostsContainer}
+            currentUser={currentUser}
+            setMakeCommentTrigger={setMakeCommentTrigger}
+            setCommentThreadTrigger={setCommentThreadTrigger}
+            postComments={postComments}
+          />
+        </div>
       </div>
-      <div className="user-posts-container">
-        {/* should have window extend when someone wants to comment */}
-        <Posts
-          createdPostsContainer={createdPostsContainer}
-          currentUser={currentUser}
-        />
-      </div>
-    </div>
+    </>
   );
 }
 
--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -4,19 +4,17 @@ import "./Posts.css";
 // images
 import deleteIcon from "../../assets/images/delete-button.png";
 
-//components
-import CommentThread from "../CommentThread/CommentThread";
-import AddCommentPopup from "../AddCommentPopup/AddCommentPopup";
-
-function Posts({ createdPostsContainer, currentUser }) {
+function Posts({
+  createdPostsContainer,
+  currentUser,
+  setMakeCommentTrigger,
+  setCommentThreadTrigger,
+  postComments,
+}) {
   // states
-  const [makeCommentTrigger, setMakeCommentTrigger] = useState(false);
-  const [commentThreadTrigger, setCommentThreadTrigger] = useState(false);
-  const [addCommentData, setAddCommentData] = useState("");
 
   //should recieve from user login
   const [postLikes, setPostLikes] = useState(0);
-  const [postComments, setPostComments] = useState([]);
 
   if (createdPostsContainer.length === 0) {
     return (
@@ -44,93 +42,72 @@ function Posts({ createdPostsContainer, currentUser }) {
     console.log(createdPostsContainer);
   };
 
-  return (
-    <>
-      {/* shouold have comment maker popup when triggered */}
-      {makeCommentTrigger && (
-        <AddCommentPopup
-          setMakeCommentTrigger={setMakeCommentTrigger}
-          addCommentData={addCommentData}
-          setAddCommentData={setAddCommentData}
-          setPostComments={setPostComments}
-        />
-      )}
-      {/* should show thread once trigger activated */}
-      {commentThreadTrigger && (
-        <CommentThread
-          postComments={postComments}
-          setCommentThreadTrigger={setCommentThreadTrigger}
-          setPostComments={setPostComments}
-        />
-      )}
-      {createdPostsContainer.map((post, index) => (
-        <div key={index} className="post">
-          <div className="post-creator-data">
-            <h4>{currentUser.username}</h4>
-            <button
-              className="deleteButton"
-              onClick={() => {
-                deletePostHandler();
-              }}
-            >
-              <img alt="x" src={deleteIcon} />
-            </button>
-          </div>
-          <div className="horizontal-divider-line" />
-          <div className="post-content-data">
-            {/* Populate post content data here */}
-            <p>{post.content}</p>
-          </div>
-          <div className="post-interactions-container">
-            {(postLikes > 0 || postComments.length > 0) && (
-              <div className="interaction-totals">
-                {/* should be uniquely based off post */}
-                {/* should only show when a comment or like even exists for the respective post */}
-                <div className="totals-likes">
-                  {postLikes > 0 && (
-                    <p>
-                      {postLikes} {postLikes === 1 ? "Like" : "Likes"}
-                    </p>
-                  )}
-                </div>
-                <div className="totals-comments">
-                  {postComments.length > 0 && (
-                    <p
-                      className="greyHover"
-                      onClick={() => {
-                        setCommentThreadTrigger(true);
-                        console.log(postComments);
-                      }}
-                    >
-                      {postComments.length}{" "}
-                      {postComments.length > 1 ? "Comments" : "Comment"}
-                    </p>
-                  )}
-                </div>
-              </div>
-            )}
-            <div className="interaction-buttons">
-              <button
-                onClick={() => {
-                  likeButtonHandler();
-                }}
-              >
-                Like
-              </button>
-              <div className="vertical-divider-line" />
-              <button
-                onClick={() => {
-                  commentButtonHandler();
-                }}
-              >
-                Comment
-              </button>
+  return createdPostsContainer.map((post, index) => (
+    <div key={index} className="post">
+      <div className="post-creator-data">
+        <h4>{currentUser.username}</h4>
+        <button
+          className="deleteButton"
+          onClick={() => {
+            deletePostHandler();
+          }}
+        >
+          <img alt="x" src={deleteIcon} />
+        </button>
+      </div>
+      <div className="horizontal-divider-line" />
+      <div className="post-content-data">
+        {/* Populate post content data here */}
+        <p>{post.content}</p>
+      </div>
+      <div className="post-interactions-container">
+        {(postLikes > 0 || postComments.length > 0) && (
+          <div className="interaction-totals">
+            {/* should be uniquely based off post */}
+            {/* should only show when a comment or like even exists for the respective post */}
+            <div className="totals-likes">
+              {postLikes > 0 && (
+                <p>
+                  {postLikes} {postLikes === 1 ? "Like" : "Likes"}
+                </p>
+              )}
+            </div>
+            <div className="totals-comments">
+              {postComments.length > 0 && (
+                <p
+                  className="greyHover"
+                  onClick={() => {
+                    setCommentThreadTrigger(true);
+                    console.log(postComments);
+                  }}
+                >
+                  {postComments.length}{" "}
+                  {postComments.length > 1 ? "Comments" : "Comment"}
+                </p>
+              )}
             </div>
           </div>
+        )}
+        <div className="interaction-buttons">
+          <button
+            onClick={() => {
+              likeButtonHandler();
+            }}
+          >
+            Like
+          </button>
+          <div className="vertical-divider-line" />
+          <button
+            onClick={() => {
+              commentButtonHandler();
+            }}
+          >
+            Comment
+          </button>
         </div>
-      ))}
-    </>
-  );
+      </div>
+    </div>
+  ));
 }
 
 export default Posts;
